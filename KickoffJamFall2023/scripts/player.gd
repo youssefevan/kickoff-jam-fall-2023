d56bb8abfdb0a73fc2d76e23cc0780778840f32f
Evan <eyoussef128@gmail.com>
2023-08-25 18:56:03 -0400

    Character Controller

--- KickoffJamFall2023/scripts/player.gd
+++ KickoffJamFall2023/scripts/player.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
+const MOVE_SPEED := 10000.0
 
+var move_input : Vector2
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	
+	handle_input()
+	
+	velocity = move_input.normalized() * MOVE_SPEED * delta
 	
 	move_and_slide()
+
+func handle_input() -> void:
+	move_input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	move_input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
