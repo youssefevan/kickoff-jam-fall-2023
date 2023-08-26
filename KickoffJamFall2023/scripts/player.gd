7114af6899f07ed973a7afe42765fc60c853dce6
Evan <eyoussef128@gmail.com>
2023-08-26 17:42:27 -0400

    Tilemap w/ collision

--- KickoffJamFall2023/scripts/player.gd
+++ KickoffJamFall2023/scripts/player.gd
@@ -13,5 +13,5 @@ func _physics_process(delta) -> void:
 	move_and_slide()
 
 func handle_input() -> void:
-	move_input.y = Input.get_action_strength("up") - Input.get_action_strength("down")
-	move_input.x = Input.get_action_strength("left") - Input.get_action_strength("right")
+	move_input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	move_input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
