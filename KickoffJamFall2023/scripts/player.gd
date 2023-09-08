a3073bfa90fe78a01de2cf383b63ee196c0d4b69
Evan <eyoussef128@gmail.com>
2023-09-08 17:52:19 -0400

    boulder movement

--- KickoffJamFall2023/scripts/player.gd
+++ KickoffJamFall2023/scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const MOVE_SPEED := 10000.0
 
+var push_force = 80.0
 var move_input : Vector2
 
 func _physics_process(delta) -> void:
@@ -9,6 +10,12 @@ func _physics_process(delta) -> void:
 	handle_input()
 	
 	velocity = move_input.normalized() * MOVE_SPEED * delta
+	
+	# This represents the player's inertia.
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 	
 	move_and_slide()
 
