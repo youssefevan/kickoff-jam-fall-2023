174059b5eea70805b5d8ea7131a8e00f254d2543
Evan <eyoussef128@gmail.com>
2023-09-08 18:46:28 -0400

    Entity class

---
+++ KickoffJamFall2023/entity.gd
@@ -0,0 +1,15 @@
+extends Area2D
+class_name Entity
+
+var tile_size = 64
+var animation_speed = 10
+var moving = false
+
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
--- KickoffJamFall2023/scripts/player_grid.gd
+++ KickoffJamFall2023/scripts/player_grid.gd
@@ -1,32 +1,25 @@
-extends Area2D
+extends Entity
 
-@onready var ray = $RayCast2D
+@onready var ray = $PhysicsRay
 
-var tile_size = 64
-var animation_speed = 10
-var moving = false
-
-var inputs = {"right": Vector2.RIGHT,
-			"left": Vector2.LEFT,
-			"up": Vector2.UP,
-			"down": Vector2.DOWN}
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
+var current_item : Item
 
 func _unhandled_input(event):
 	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
+		if event.is_action_pressed(dir) and moving == false:
 			move(dir)
 
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
-		var tween = create_tween()
-		tween.tween_property(self, "position", position + inputs[dir] *    tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
-		moving = true
-		await tween.finished
-		moving = false
+		movement_tween(dir)
+	elif ray.get_collision_mask_value(3):
+		pass
+
+func movement_tween(dir):
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+	moving = true
+	await tween.finished
+	moving = false
