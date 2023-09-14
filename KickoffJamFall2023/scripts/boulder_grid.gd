059e28fe59c531092fb173f43f1757eaefc5a8d3
Evan <eyoussef128@gmail.com>
2023-09-14 13:01:00 -0400

    Boulder can now be pushed

--- KickoffJamFall2023/scripts/boulder_grid.gd
+++ KickoffJamFall2023/scripts/boulder_grid.gd
@@ -1,3 +1,11 @@
 extends Entity
 class_name Item
 
+@onready var ray = $PhysicsRay
+
+func push(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	
+	if !ray.is_colliding():
+		movement_tween(dir)
--- KickoffJamFall2023/scripts/player_grid.gd
+++ KickoffJamFall2023/scripts/player_grid.gd
@@ -1,7 +1,6 @@
 extends Entity
 
 @onready var ray = $PhysicsRay
-var extend_ray = false
 
 var current_item : Item
 
@@ -11,20 +10,12 @@ func _unhandled_input(event):
 			move(dir)
 
 func move(dir):
-	if extend_ray == true:
-		ray.target_position = inputs[dir] * tile_size * 2
-	else:
-		ray.target_position = inputs[dir] * tile_size
+	ray.target_position = inputs[dir] * tile_size
 	
 	ray.force_raycast_update()
 	if !ray.is_colliding():
 		movement_tween(dir)
-		extend_ray = false
 	elif ray.get_collider() is TileMap:
-		extend_ray = false
-	elif ray.get_collider().get_collision_layer_value(3):
-		extend_ray == true
-		push(ray.get_collider())
-
-func push(collider : Entity):
-	print('push')
+		pass
+	elif ray.get_collider() is Entity:
+		ray.get_collider().push(dir)
